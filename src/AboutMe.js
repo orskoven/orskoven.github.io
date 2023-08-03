0d8613a9bdbae500fb7cb7f9c27ab53747325858
orskoven <simo26k2@stud.kea.dk>
2023-08-03 20:46:40 +0200

    new projects

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,8 +4,6 @@ const AboutMe = () => {
             <h2>I❤️‍🔥 Computers</h2>
             <p>A student of code, an explorer of bytes, I traverse the landscapes of technology. Inspired by the great Hemingway's brevity and wit, I shape the world of digital narratives. Whether it's in class as an engaged computer science student or in my YouTube tutorials at <a href="https://www.youtube.com/@spilledbeans" class="youtube-link">SpilledBeans</a>, I unravel the complexity of coding into simple truths.</p>
             <p>Extroverted and a team player, I thrive in collaboration. My skills are not just confined to the screen, but extend to relationships, building connections, one code line at a time.</p>
-            <p>I'm an aspiring computer science AP student, a lover of Apple, coding,           tech, and the endless wonders of the internet. In the quiet corners of my room, you'll find me playing a soft melody on the guitar or diving into a gripping novel. But when I lace up my running shoes or indulge in a movie, my love for life's simpler pleasures shines through.
-</p>
          </section>
     );
   };
--- src/Projects.js
+++ src/Projects.js
@@ -1,15 +1,26 @@
 const Projects = () => {
-    return (
-      <section className="section">
-        <h2>Projects</h2>
-        <div className="project">
-          <h3>Project Name</h3>
-          <p>Description of the project. What it does, the technologies used, etc.</p>
-        </div>
-        {/* Add more projects as needed */}
-      </section>
-    );
-  };
-  
-  export default Projects;
-  
+  return (
+    <section className="section">
+      <h2>Projects</h2>
+      {renderProject('Spilled Beans (YouTube Kanal)', 'nov. 2021 - nu', 'Content creator')}
+      {renderProject('GitHub', 'aug. 2021 - nu', 'Content creator')}
+      {renderProject('Morfar - afgangsfilm', 'jun. 2018', `The movie is a window into the world of the director's 85-year-old grandfather...`)}
+      {renderProject('Betzavta - Learning the Language of Democracy', 'nov. 2013', 'Betzavta, which took place for a week in Wroclaw (Poland), is an internationally recognized education...')}
+      {renderProject('Social Media & Youth - Learning for Employment', 'okt. 2013', 'I attended a seminar in Bucharest, Romania, to debate and explore how digital skills...')}
+      {renderProject('Free2Choose Create - Nordic', 'jul. 2013', 'The Free2choose-Create course took place in Uppsala, Sweden, with the aim of combining human rights education...')}
+    </section>
+  );
+};
+
+const renderProject = (title, date, description) => {
+  return (
+    <div className="project-tile">
+      <h3>{title}</h3>
+      <span className="date">{date}</span>
+      <p className="description">{description}</p>
+      <button className="view-project-button">Vis projekt</button>
+    </div>
+  );
+};
+
+export default Projects;
