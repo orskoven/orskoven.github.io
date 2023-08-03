0a191e481a1f77b63eadda20db9dd6c7779c3e5a
Simon Dieckmann Ørskov Beckmann <90264457+orskoven@users.noreply.github.com>
2023-08-03 13:18:20 +0200

    Update AboutMe.js

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,7 +1,7 @@
 const AboutMe = () => {
     return (
       <section className="section">
-            <h2>I ❤️‍🔥 Computers</h2>
+            <h2>I❤️‍🔥👨🏼‍💻</h2>
             <p>A student of code, an explorer of bytes, I traverse the landscapes of technology. Inspired by the great Hemingway's brevity and wit, I shape the world of digital narratives. Whether it's in class as an engaged computer science student or in my YouTube tutorials at <a href="https://www.youtube.com/@spilledbeans" class="youtube-link">SpilledBeans</a>, I unravel the complexity of coding into simple truths.</p>
             <p>Extroverted and a team player, I thrive in collaboration. My skills are not just confined to the screen, but extend to relationships, building connections, one code line at a time.</p>
          </section>
