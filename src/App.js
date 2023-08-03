4392617f9784910840abf2bdf4d35570d84e3f34
orskoven <simo26k2@stud.kea.dk>
2023-08-03 18:29:27 +0200

    aboutme change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ const App = () => {
       <header className="header">
         <h1>Welcome to Simon's Portfolio</h1>
         <p className="intro-text">
-          I'm an aspiring computer science AP student, a lover of Apple, coding, tech, and the endless wonders of the internet. In the quiet corners of my room, you'll find me playing a soft melody on the guitar or diving into a gripping novel. But when I lace up my running shoes or indulge in a movie, my love for life's simpler pleasures shines through.
         </p>
       </header>
       <main className="content">
