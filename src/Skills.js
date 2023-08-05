6fbd670361b5f87572eaf9ef11e259688145f7c2
Simon Dieckmann Ørskov Beckmann <90264457+orskoven@users.noreply.github.com>
2023-08-05 08:02:41 +0200

    Update Skills.js

--- src/Skills.js
+++ src/Skills.js
@@ -1,65 +1,50 @@
 const Skills = () => {
-  const skillsCategories = {
-    'Programming Languages': [
-      '✨ JavaScript',
-      '🐍 Python',
-      '☕ JAVA',
-      '📱 Swift (Programming Language)',
-      'C',
-      'Machine Code',
-    ],
-    Frameworks: [
-      '🔮 React.js',
-      '🌼 Spring Framework',
-      '🌱 Spring Boot',
-      '👢 Bootstrap',
-      '🐍 Django',
-      '🍶 Flask',
-    ],
-    'Database & Storage': ['🗃️ MySQL', '🔍 SQL', '📂 NoSQL'],
-    'Machine Learning & Data Analysis': [
-      '🤖 Tensorflow',
-      '🧠 PyTorch',
-      '📈 Data Analysis',
-      '🐼 Pandas (Software)',
-      '📊 Seaborn',
-    ],
-    'Web & Mobile Development': [
-      '🎨 Cascading Style Sheets (CSS)',
-      '🖥️ HTML5',
-      '📱 iOS-udvikling',
-      '💧 Agile & Waterfall Methodologies',
-    ],
-    'Other Skills': [
-      '🔄 Agile Methodologies',
-      '🎙️ Public Speaking',
-      '⚡ Lambda Expressions',
-      '🔐 Cybersikkerhed',
-      '🗣️ Presentations',
-      '✅ Unit Testing',
-      '💨 Extreme Programming',
-      '📐 GoF Patterns',
-      '🧩 Object-Oriented Programming (OOP)',
-      '🔍 Analytical Skills',
-    ],
-  };
-
   return (
     <section className="section">
       <h2>Skills</h2>
-      {Object.keys(skillsCategories).map((category) => (
-        <div className="category" key={category}>
-          <h3>{category}</h3>
-          <div className="skills-container">
-            {skillsCategories[category].map((skill) => (
-              <span className="skill" key={skill}>
-                {skill}
-              </span>
-            ))}
-          </div>
-        </div>
-      ))}
+      {renderSkillCategory("Frontend Development", [
+        "🔮 React", "✨ JavaScript", "🎨 CSS", "👢 Bootstrap", "📱 iOS-udvikling",
+      ])}
+      {renderSkillCategory("Backend Development", [
+        "🗃️ MySQL", "☕ JAVA", "🌱 Spring Boot", "👢 Bootstrap", "🌿 Django", "🐘 SQL", "🔮 NoSQL",
+      ])}
+      {renderSkillCategory("Machine Learning", [
+        "🤖 Tensorflow", "🧠 Keras", "🔥 PyTorch", "🔬 Machine Learning", "🐍 Pandas (Software)",
+      ])}
+      {renderSkillCategory("Agile & Methodologies", [
+        "🏃‍♂️ Agile Methodologies", "🌊 Waterfall Methodologies", "🏁 Scrum", "⚡ Extreme Programming",
+      ])}
+      {renderSkillCategory("Testing & Frameworks", [
+        "🧪 Unit Testing", "🌸 Spring Framework",
+      ])}
+      {renderSkillCategory("Programming Languages", [
+        "🐍 Python", "☕ Java", "🚀 GoF Patterns", "🧩 Object-Oriented Programming (OOP)", "🐦 Swift (Programming Language)",
+      ])}
+      {renderSkillCategory("Data & Security", [
+        "📊 Data Analysis", "🔒 Cybersikkerhed", "🔐 Seaborn",
+      ])}
+      {renderSkillCategory("Web Technologies", [
+        "🕸️ HTML5", "👢 Bootstrap", "🔥 Firebase", "🌱 Spring Boot",
+      ])}
+      {renderSkillCategory("Others", [
+        "🎤 Public Speaking", "📊 UML", "🧠 RUP", "🧮 Lambda Expressions", "🎨 CSS", "📈 Presentations", "📚 Database Development", "🧠 Gestalt", "🍼 Flask",
+      ])}
     </section>
+  );
+};
+
+const renderSkillCategory = (title, skills) => {
+  return (
+    <div className="skills-category">
+      <h3>{title}</h3>
+      <div className="skills-slider">
+        {skills.map((skill, index) => (
+          <div className="skill-tile" key={index}>
+            {skill}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
