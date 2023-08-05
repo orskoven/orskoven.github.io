dfa8880d494aa78ddfe1addd89e368b02052b41b
Simon Dieckmann Ørskov Beckmann <90264457+orskoven@users.noreply.github.com>
2023-08-05 07:53:08 +0200

    Update Skills.js

--- src/Skills.js
+++ src/Skills.js
@@ -1,29 +1,66 @@
 const Skills = () => {
-    return (
-      <section className="section">
-        <h2>Skills</h2>
-        {renderSkillCategory("Frontend Development", ["🔮 React", "✨ JavaScript", "🎨 CSS", "👢 Bootstrap"])}
-        {renderSkillCategory("Backend Development", ["🗃️ MySQL", "☕ JAVA", "🌱 Spring Boot", "📱 Swift"])}
-        {renderSkillCategory("Machine Learning", ["🤖 Tensorflow", "🧠 Keras"])}
-        {renderSkillCategory("Low-Level Programming", ["🔌 Machine Code", "📚 C"])}
-      </section>
-    );
+  const skillsCategories = {
+    'Programming Languages': [
+      '✨ JavaScript',
+      '🐍 Python',
+      '☕ JAVA',
+      '📱 Swift (Programming Language)',
+      'C',
+      'Machine Code',
+    ],
+    Frameworks: [
+      '🔮 React.js',
+      '🌼 Spring Framework',
+      '🌱 Spring Boot',
+      '👢 Bootstrap',
+      '🐍 Django',
+      '🍶 Flask',
+    ],
+    'Database & Storage': ['🗃️ MySQL', '🔍 SQL', '📂 NoSQL'],
+    'Machine Learning & Data Analysis': [
+      '🤖 Tensorflow',
+      '🧠 PyTorch',
+      '📈 Data Analysis',
+      '🐼 Pandas (Software)',
+      '📊 Seaborn',
+    ],
+    'Web & Mobile Development': [
+      '🎨 Cascading Style Sheets (CSS)',
+      '🖥️ HTML5',
+      '📱 iOS-udvikling',
+      '💧 Agile & Waterfall Methodologies',
+    ],
+    'Other Skills': [
+      '🔄 Agile Methodologies',
+      '🎙️ Public Speaking',
+      '⚡ Lambda Expressions',
+      '🔐 Cybersikkerhed',
+      '🗣️ Presentations',
+      '✅ Unit Testing',
+      '💨 Extreme Programming',
+      '📐 GoF Patterns',
+      '🧩 Object-Oriented Programming (OOP)',
+      '🔍 Analytical Skills',
+    ],
   };
-  
-  const renderSkillCategory = (title, skills) => {
-    return (
-      <div className="skills-category">
-        <h3>{title}</h3>
-        <div className="skills-slider">
-          {skills.map((skill, index) => (
-            <div className="skill-tile" key={index}>
-              {skill}
-            </div>
-          ))}
+
+  return (
+    <section className="section">
+      <h2>Skills</h2>
+      {Object.keys(skillsCategories).map((category) => (
+        <div className="category" key={category}>
+          <h3>{category}</h3>
+          <div className="skills-container">
+            {skillsCategories[category].map((skill) => (
+              <span className="skill" key={skill}>
+                {skill}
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
-    );
-  };
-  
-  export default Skills;
-  
+      ))}
+    </section>
+  );
+};
+
+export default Skills;
