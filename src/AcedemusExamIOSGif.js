ca2d03fe1de6abbb122a96ca3b7ba8bb257548ae
Simon Dieckmann Ørskov Beckmann <90264457+orskoven@users.noreply.github.com>
2023-11-06 14:42:28 +0100

    Update AcedemusExamIOSGif.js

--- src/AcedemusExamIOSGif.js
+++ src/AcedemusExamIOSGif.js
@@ -4,7 +4,7 @@ const AcedemusExamIOSGif = () => {
   return (
     <section className="section">
       <h2>Acedemus: Study IOS APP</h2>
-      <img src="/Users/simonpedersen/Documents/GitHub/orskoven.github.io/src/AcedemusExamIOSGif.gif" alt="Acedemus GIF" />
+      <img src="orskoven.github.io/src/AcedemusExamIOSGif.gif" alt="Acedemus GIF" />
     </section>
   );
 };
